311aee6a805723d08701aeb96088e8d0624e0b67
AxAd <asadhusn007@gmail.com>
2024-07-27 16:18:30 +0500

    added dark theme

--- common.js
+++ common.js
@@ -1,12 +1,17 @@
 // const URL = "http://localhost:3000";
 const URL = "https://restcountries.com/v3.1";
 
+const theme = localStorage.getItem("THEME");
+if (theme === "dark") document.body.classList.add(theme);
+
 document.getElementById("toggleTheme").onclick = function () {
   this.firstElementChild.classList.toggle("fa-solid");
-  document.body.classList.contains("dark")
+  const theme = document.body.classList.contains("dark") ? null : "dark";
+  theme
     ? (this.lastChild.textContent = " Dark Mode")
     : (this.lastChild.textContent = " Light Mode");
   document.body.classList.toggle("dark");
+  localStorage.setItem("THEME", theme);
 };
 
 function createElement(name) {
